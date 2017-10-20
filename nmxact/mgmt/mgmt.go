cc796b52fbae255f8e364790aa2e9baf499154ef
Marko Kiiskila <marko@runtime.io>
2017-10-19 17:39:07 -0700

    nmxact; image upload with oic would always try to use Coap over TCP encoding.

--- nmxact/mgmt/mgmt.go
+++ nmxact/mgmt/mgmt.go
@@ -27,37 +27,20 @@ import (
 	"mynewt.apache.org/newtmgr/nmxact/sesn"
 )
 
-func MtuNmp(rawMtu int) int {
-	return rawMtu - nmp.NMP_HDR_SIZE
-}
-
-func MtuOmp(rawMtu int) int {
-	return rawMtu - omp.OMP_MSG_OVERHEAD - nmp.NMP_HDR_SIZE
-}
-
-func MtuMgmt(rawMtu int, mgmtProto sesn.MgmtProto) (int, error) {
-	switch mgmtProto {
-	case sesn.MGMT_PROTO_NMP:
-		return MtuNmp(rawMtu), nil
-
-	case sesn.MGMT_PROTO_OMP:
-		return MtuOmp(rawMtu), nil
-
-	default:
-		return 0, fmt.Errorf("invalid management protocol: %+v", mgmtProto)
-	}
-}
-
-func EncodeMgmt(mgmtProto sesn.MgmtProto, m *nmp.NmpMsg) ([]byte, error) {
-	switch mgmtProto {
+func EncodeMgmt(s sesn.Sesn, m *nmp.NmpMsg) ([]byte, error) {
+	switch s.MgmtProto() {
 	case sesn.MGMT_PROTO_NMP:
 		return nmp.EncodeNmpPlain(m)
 
 	case sesn.MGMT_PROTO_OMP:
-		return omp.EncodeOmpTcp(m)
+		if s.CoapIsTcp() == true {
+			return omp.EncodeOmpTcp(m)
+		} else {
+			return omp.EncodeOmpDgram(m)
+		}
 
 	default:
 		return nil,
-			fmt.Errorf("invalid management protocol: %+v", mgmtProto)
+			fmt.Errorf("invalid management protocol: %+v", s.MgmtProto())
 	}
 }
