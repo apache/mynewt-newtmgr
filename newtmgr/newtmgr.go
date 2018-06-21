934730617f37733e10103d2faea7fc4ca5699bcc
Szymon Janc <szymon.janc@codecoup.pl>
2018-06-21 13:23:40 +0200

    Release 1.4.1

--- newtmgr/newtmgr.go
+++ newtmgr/newtmgr.go
@@ -77,7 +77,7 @@ func main() {
 		ExeName:       "newtmgr",
 		ShortName:     "Newtmgr",
 		LongName:      "Apache Newtmgr",
-		VersionString: "1.4.0",
+		VersionString: "1.4.1",
 		CfgFilename:   ".newtmgr.cp.json",
 	}
 
