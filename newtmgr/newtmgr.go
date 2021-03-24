f26e34eb5861bd310a24e12e32adfe7a8f7d1f43
Szymon Janc <szymon.janc@codecoup.pl>
2021-03-24 15:08:52 +0100

    Apache Mynewt 1.9.0 release

--- newtmgr/newtmgr.go
+++ newtmgr/newtmgr.go
@@ -77,7 +77,7 @@ func main() {
 		ExeName:       "newtmgr",
 		ShortName:     "Newtmgr",
 		LongName:      "Apache Newtmgr",
-		VersionString: "1.9.0-dev",
+		VersionString: "1.10.0-dev",
 		CfgFilename:   ".newtmgr.cp.json",
 	}
 
