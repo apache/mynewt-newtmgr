c3cc444a278471b5ca5293607526e05295ded0e4
Szymon Janc <szymon.janc@codecoup.pl>
2022-04-22 10:18:06 +0200

    Prepare for Mynewt 1.10.0 release

--- newtmgr/newtmgr.go
+++ newtmgr/newtmgr.go
@@ -77,7 +77,7 @@ func main() {
 		ExeName:       "newtmgr",
 		ShortName:     "Newtmgr",
 		LongName:      "Apache Newtmgr",
-		VersionString: "1.10.0-dev",
+		VersionString: "1.10.0",
 		CfgFilename:   ".newtmgr.cp.json",
 	}
 
